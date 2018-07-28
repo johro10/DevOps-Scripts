24dbd84bba7cdbf9c1e79e176fdb57b02ee122b0
Rob Johnson <robert.johnson680@gmail.com>
2018-07-28 14:36:39 -0400

    chg

--- Python/MVAModule5Dates.py
+++ Python/MVAModule5Dates.py
@@ -1,4 +1,6 @@
 #The import statement gives us access to the functionality of the datetime class
 import datetime
+currentDate = (datetime.date.today())
 #today is a function that returns today's date
-print(datetime.date.today())
+print(currentDate)
+print(currentDate.strftime('%d %b,%Y'))
