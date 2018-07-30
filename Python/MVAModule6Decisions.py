3e1dd7ad7943d04b337ac95979f275c684a90338
Rob Johnson <robert.johnson680@gmail.com>
2018-07-30 13:11:32 -0400

    chg

---
+++ Python/MVAModule6Decisions.py
@@ -0,0 +1,5 @@
+positiveAnswer = 'Yes'
+answer = input('Would you like express shipping?')
+if answer.capitalize() == positiveAnswer.capitalize():
+    print('That will be an extra $10')
+print('Have a nice day, :-)')
---
+++ Python/mvaModule8CmplxDecisions.py
@@ -0,0 +1,7 @@
+team = input('Enter your favorite team: ').capitalize()
+sport = input('Enter your favorite sport: ').capitalize()
+
+if sport == 'Hockey' and team == 'Rangers' :
+    print('i miss messier')
+else:
+    print('I don\'t know that team')
